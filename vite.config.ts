810d83beaf3dfea4c1ee904c20b64d2c39344735
msontl20-alt <msontl20@gmail.com>
2026-05-14 17:13:50 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({mode}) => {
   return {
     plugins: [react(), tailwindcss()],
     define: {
+      base:'/autonx/',
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
